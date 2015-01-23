55a40d0d009b6436d32dd783b25e375232bd49fe
Andrew Theken <theken.1@osu.edu>
2015-01-23 15:58:30 -0500

    Factoring send/batch into a client 'class'.

--- tests/fixture_postmarkClientSending.js
+++ tests/fixture_postmarkClientSending.js
@@ -1,13 +1,38 @@
 var mocha = require('mocha');
 var assert = require('assert');
+var testingKeys = require('./testing_keys.json');
+var util = require('util');
 
-describe('client', function(done){
-	describe('send', function(done){
-		/*
-		it('should return the message status.', function(done){
-			//assert.notEqual(null,null)
-			done();
-		});
-		*/
-	});
+var postmark = require('../lib/postmark/index.js');
+
+describe('client', function() {
+  var _client = null;
+
+  beforeEach(function() {
+    _client = new postmark.Client(testingKeys.WRITE_TEST_SERVER_TOKEN);
+  });
+
+  it('should send a single email', function(done) {
+    _client.send({
+      to: testingKeys.WRITE_TEST_EMAIL_RECIPIENT_ADDRESS,
+      from: testingKeys.WRITE_TEST_SENDER_EMAIL_ADDRESS,
+      subject: "Hello from the node.js client! " + new Date(),
+      textBody: "Testing 1.2.3..."
+    }, done);
+  });
+
+  it('should send a batch of emails', function(done) {
+    var emailBatch = [];
+
+    for (var i = 0; i < 5; i++) {
+      emailBatch.push({
+        to: testingKeys.WRITE_TEST_EMAIL_RECIPIENT_ADDRESS,
+        from: testingKeys.WRITE_TEST_SENDER_EMAIL_ADDRESS,
+        subject: "Hello from the node.js client! " + new Date(),
+        textBody: "Testing batch email: " + i
+      });
+    }
+    _client.batch([emailBatch], done);
+  });
+
 });
