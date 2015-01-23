01c0edf71e011d03943b1edee14c6345a8a3ff81
Andrew Theken <theken.1@osu.edu>
2015-01-23 09:29:18 -0500

    Adding Mocha Support.

---
+++ tests/fixture_postmarkClientSending.js
@@ -0,0 +1,13 @@
+var mocha = require('mocha');
+var assert = require('assert');
+
+describe('client', function(done){
+	describe('send', function(done){
+		/*
+		it('should return the message status.', function(done){
+			//assert.notEqual(null,null)
+			done();
+		});
+		*/
+	});
+});
