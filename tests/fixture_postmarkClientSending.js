ae040c5402ab44ba28d2dd24ad3b46aaac020151
Andrew Theken <theken.1@osu.edu>
2015-01-29 09:47:18 -0500

    Fixing false positives in tests, adding User-Agent info to request.

--- tests/fixture_postmarkClientSending.js
+++ tests/fixture_postmarkClientSending.js
@@ -2,14 +2,25 @@ var mocha = require('mocha');
 var assert = require('assert');
 var testingKeys = require('./testing_keys.json');
 var util = require('util');
+var merge = require('merge');
 
 var postmark = require('../lib/postmark/index.js');
 
 describe('client', function() {
+  // allow some of the more intensive tests to take longer.
+  this.timeout(30000);
   var _client = null;
 
   beforeEach(function() {
     _client = new postmark.Client(testingKeys.WRITE_TEST_SERVER_TOKEN);
+  });
+
+  it('should return message opens', function(done) {
+    //var client = postmark(testingKeys.WRITE_TEST_SERVER_TOKEN);
+    _client.getMessageOpens({
+      count: 10
+    }, done);
+
   });
 
   describe('"legacy" initialization support', function() {
@@ -30,10 +41,10 @@ describe('client', function() {
 
   it('should send a single email', function(done) {
     _client.send({
-      to: testingKeys.WRITE_TEST_EMAIL_RECIPIENT_ADDRESS,
-      from: testingKeys.WRITE_TEST_SENDER_EMAIL_ADDRESS,
-      subject: "Hello from the node.js client! " + new Date(),
-      textBody: "Testing 1.2.3..."
+      To: testingKeys.WRITE_TEST_EMAIL_RECIPIENT_ADDRESS,
+      From: testingKeys.WRITE_TEST_SENDER_EMAIL_ADDRESS,
+      Subject: "Hello from the node.js client! " + new Date(),
+      TextBody: "Testing 1.2.3..."
     }, done);
   });
 
@@ -48,6 +59,6 @@ describe('client', function() {
         textBody: "Testing batch email: " + i
       });
     }
-    _client.batch([emailBatch], done);
+    _client.batch(emailBatch, done);
   });
 });
