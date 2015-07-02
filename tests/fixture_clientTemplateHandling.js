13cf121e5a95f0cb0b292214741e81790eeea91f
Andrew Theken <andrew@attr.io>
2015-07-02 18:24:46 -0400

    Adding support for template API.

--- tests/fixture_clientTemplateHandling.js
+++ tests/fixture_clientTemplateHandling.js
@@ -17,27 +17,94 @@ describe('client template handling', function() {
         _client = new postmark.Client(testingKeys.get('WRITE_TEST_SERVER_TOKEN'));
     });
 
-    // it('should retrieve a list of templates.', function(done) {
-    //     done();
-    // });
+    after(function() {
+        _client.getTemplates(function(err, results) {
 
-    // it('should get a single template.', function(done) {
-    //     done();
-    // });
+            while (results.Templates.length > 0) {
+                var t = results.Templates.pop();
+                if (/testing-template-node-js/.test(t.Name)) {
+                    _client.deleteTemplate(t.TemplateId);
+                }
+            }
+        });
+    });
 
-    // it('should get a update a template.', function(done) {
-    //     done();
-    // });
+    it('should retrieve a list of templates.', function(done) {
+        _client.getTemplates(done);
+    });
 
-    // it('should get a create a template.', function(done) {
-    //     done();
-    // });
+    it('should get a single template.', function(done) {
+        _client.createTemplate({
+            name: "testing-template-node-js" + Date(),
+            textBody: "text body for template {{id}}!",
+            htmlBody: "{{content}}",
+            subject: "{{subject}}"
+        }, function(err, template) {
+            _client.getTemplate(template.TemplateId, done);
+        });
+    });
 
-    // it('should get a delete a template.', function(done) {
-    //     done();
-    // });
+    it('should a update a template.', function(done) {
+        _client.createTemplate({
+            name: "testing-template-node-js" + Date(),
+            textBody: "text body for template {{id}}!",
+            htmlBody: "{{content}}",
+            subject: "{{subject}}"
+        }, function(error, newTemplate) {
+            _client.editTemplate(newTemplate.TemplateId, {
+                name: "testing-template-node-js" + Date(),
+                textBody: "text body for template {{id}}!",
+                htmlBody: "{{content}}",
+                subject: "{{subject}}"
+            }, done);
+        });
+    });
 
-    // it('send an email using a template.', function(done) {
-    //     done();
-    // });
+    it('should a create a template.', function(done) {
+        _client.createTemplate({
+            name: "testing-template-node-js" + Date(),
+            textBody: "text body for template {{id}}!",
+            htmlBody: "{{content}}",
+            subject: "{{subject}}"
+        }, done);
+    });
+
+    it('should a delete a template.', function(done) {
+        _client.createTemplate({
+            name: "testing-template-node-js" + Date(),
+            textBody: "text body for template {{id}}!",
+            htmlBody: "{{content}}",
+            subject: "{{subject}}"
+        }, function(err, template) {
+            _client.deleteTemplate(template.TemplateId, done);
+        });
+    });
+
+    it('send an email using a template.', function(done) {
+        _client.createTemplate({
+            name: "testing-template-node-js" + Date(),
+            textBody: "text body for template {{id}}!",
+            htmlBody: "{{content}}",
+            subject: "{{subject}}"
+        }, function(error, t) {
+            _client.sendEmailWithTemplate({
+                To: testingKeys.get('WRITE_TEST_EMAIL_RECIPIENT_ADDRESS'),
+                From: testingKeys.get('WRITE_TEST_SENDER_EMAIL_ADDRESS'),
+                TemplateId: t.TemplateId,
+                TemplateModel: {}
+            }, done);
+        });
+    });
+
+    it('should validate template', function(done) {
+        _client.validateTemplate({
+            testRenderModel: {
+                Name: "joe!"
+            },
+            textBody: "text body for template {{id}}!",
+            htmlBody: "{{content}}",
+            subject: "{{subject}}"
+        }, done);
+    })
+
 });
