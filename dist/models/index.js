b6e31d252acd540a6eb8a3ebee18ad7b3e23fc8d
Igor Balos <ibalosh.testing@gmail.com>
2018-08-02 07:34:58 +0200

    removed for now dist folder

--- dist/models/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var SupportingTypes_1 = require("./SupportingTypes");
-exports.HttpMethod = SupportingTypes_1.HttpMethod;
-var PostmarkError_1 = require("./PostmarkError");
-exports.PostmarkError = PostmarkError_1.default;
