0104b252a25b0218119bbae0a44729ffc252cff3
Igor Balos <ibalosh@gmail.com>
2022-08-29 13:50:07 +0200

    added message stream filter parameter to stats endpoint

--- src/client/models/stats/StatsFilteringParameters.ts
+++ src/client/models/stats/StatsFilteringParameters.ts
@@ -2,9 +2,12 @@ export class StatisticsFilteringParameters {
     public tag?: string;
     public fromDate?: string;
     public toDate?: string;
-    constructor(tag?: string, fromDate?: string, toDate?: string) {
+    public messageStream?: string;
+
+    constructor(tag?: string, fromDate?: string, toDate?: string, messageStream?: string) {
         this.tag = tag;
         this.fromDate = fromDate;
         this.toDate = toDate;
+        this.messageStream = messageStream;
     }
 }
