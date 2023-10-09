87ca177db7217300e5416800e3c053320218fb4e
Igor Balos <ibalosh@gmail.com>
2023-10-09 14:30:56 +0200

    add tests

---
+++ test/integration/DataRemoval.test.ts
@@ -0,0 +1,35 @@
+import {expect} from "chai";
+import "mocha";
+
+import {DataRemovalStatus, DataRemovalStatusTypes} from "../../src/client/models";
+import * as postmark from "../../src/index";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+describe("DataRemoval", () => {
+    const accountToken: any = process.env.ACCOUNT_API_TOKEN;
+    const client = new postmark.AccountClient(accountToken);
+    const fromAddress: any = process.env.SENDER_EMAIL_ADDRESS;
+
+    it("createDataRemoval", async () => {
+        const dataRemovalStatus:DataRemovalStatus = await client.createDataRemoval({
+            RequestedBy: fromAddress.toString(),
+            RequestedFor: 'test@example.com',
+            NotifyWhenCompleted: false})
+
+        expect(dataRemovalStatus.Status).to.eq(DataRemovalStatusTypes.Pending);
+        expect(dataRemovalStatus.ID).to.be.gt(0);
+    });
+
+    it("getDataRemoval", async () => {
+        let dataRemovalStatus: DataRemovalStatus = await client.createDataRemoval({
+            RequestedBy: fromAddress.toString(),
+            RequestedFor: 'test@example.com',
+            NotifyWhenCompleted: false})
+
+        const dataRemovalStatusId:number = dataRemovalStatus.ID;
+        dataRemovalStatus = await client.getDataRemoval(dataRemovalStatusId);
+        expect(dataRemovalStatus.ID).to.eq(dataRemovalStatusId)
+    });
+});
