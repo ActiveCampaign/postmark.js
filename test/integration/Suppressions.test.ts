6887a0b58f1f319cc840dc8e237b27075fd20035
Igor Balos <ibalosh@gmail.com>
2023-11-14 12:15:33 +0100

    updated tests

--- test/integration/Suppressions.test.ts
+++ test/integration/Suppressions.test.ts
@@ -1,25 +1,25 @@
 import * as postmark from "../../src/index";
 
-import { expect } from "chai";
+import {expect} from "chai";
 import "mocha";
 
-import {Suppression, Suppressions, SuppressionStatuses} from "../../src/client/models";
+import {SuppressionOrigin, SuppressionReason, Suppressions, SuppressionStatuses} from "../../src/client/models";
 import * as dotenv from "dotenv";
+
 dotenv.config();
 
 describe("Client - Suppressions", () => {
     const serverToken: any = process.env.SERVER_API_TOKEN;
-    console.log(serverToken)
     const client = new postmark.ServerClient(serverToken);
     const suppression_email_domain:string = 'supression.example.com';
 
     async function cleanup() {
         const suppressions: Suppressions = await client.getSuppressions('outbound');
-        suppressions.Suppressions.forEach( async (suppression: Suppression) => {
+        for (const suppression of suppressions.Suppressions) {
             if (suppression.EmailAddress.includes(suppression_email_domain)) {
                 await client.deleteSuppressions('outbound', { Suppressions: [ { EmailAddress: suppression.EmailAddress} ] });
             }
-        });
+        }
     }
 
     before(cleanup);
@@ -40,9 +40,28 @@ describe("Client - Suppressions", () => {
             Suppressions: [ { EmailAddress: emailAddress} ]
         });
 
-
         const suppressions: Suppressions = await client.getSuppressions('outbound');
         expect(suppressions.Suppressions.length).to.be.gte(1);
+    });
+
+    it("getSuppressions - filter", async () => {
+        const email = `nothing+list@${suppression_email_domain}`;
+        await client.createSuppressions('outbound', {
+            Suppressions: [ { EmailAddress: email} ]
+        });
+
+        let suppressions = await client.getSuppressions('outbound', {
+            emailAddress: email,
+            origin: SuppressionOrigin.Customer,
+            suppressionReason: SuppressionReason.ManualSuppression
+        });
+        expect(suppressions.Suppressions.length).to.eq(1);
+
+        suppressions = await client.getSuppressions('outbound', {emailAddress: "invalid"});
+        expect(suppressions.Suppressions.length).to.be.eq(0);
+
+        suppressions = await client.getSuppressions('outbound', {emailAddress: email, origin: SuppressionOrigin.Admin});
+        expect(suppressions.Suppressions.length).to.eq(0);
     });
 
     it("deleteSuppression", async () => {
